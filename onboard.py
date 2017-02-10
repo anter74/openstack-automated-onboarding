cdb69960105332f27bc11435e92b70c153623eaa
Kyle Greenwell <kyle.greenwell@verizonwireless.com>
2017-02-10 11:15:41 -0500

    Token functions now use OS_AUTH_URL env var

--- onboard.py
+++ onboard.py
@@ -15,8 +15,8 @@ from calendar import timegm
 from datetime import datetime
 
 # Get initial token from Keystone API
-def getToken(url):
-    url = url + '/tokens'
+def getToken():
+    url = os.environ['OS_AUTH_URL'] + '/tokens'
     headers = {'Content-Type': 'application/json'}
 
     http = urllib3.PoolManager(
@@ -50,14 +50,20 @@ def getToken(url):
         return failure
 
 # Check token for freshness and renew if necessary
-def refreshToken(url, token):
+def refreshToken(token):
     #If current time is greater or equal to epoch time of token's expiration, call getToken to refresh
     if time.time() >= str(datetime.strptime(token['expires'], "%Y-%m-%dT%H:%M:%SZ")):
-        token = getToken(url)
+        token = getToken(os.environ['OS_AUTH_URL'])
         return token
     #Else, return the existing token back
     else:
         return token
+
+#Retrieve Correct Service URL for API Call
+def getServiceURL(token, service):
+    token = refreshToken(token)
+    urlBuilder = [i['publicURL'] for i in token['services'] if i['name'] == service]
+    return str(urlBuilder[0])
 
 def main():
     #Create jinja2 environment for rendering heat templates
@@ -68,11 +74,20 @@ def main():
     templateVars = yaml.load(stream)
     stream.close()
     #Render jinja2 template to create heat template
-    print template.render(templateVars)
+    #print template.render(templateVars)
 
-    URL = 'https://10.75.25.138:13000/v2.0'
-    token = getToken(URL)
+    token = getToken()
     if str(token['code']) == "200":
+        headers = {'Content-Type': 'application/json', 'X-Auth-Token': str(token['token_id'])}
+        http = urllib3.PoolManager(
+            cert_reqs = 'CERT_REQUIRED',
+            ca_certs = os.environ['OS_CACERT'])
+        novaListURL = str(getServiceURL(token,"nova")) + "/os-hypervisors/statistics"
+        novaList = http.request(
+            'GET',
+            novaListURL,
+            headers=headers)
+        print json.loads(novaList.data)
 
     else:
         print "Response Code: %s" % token['code']
@@ -81,17 +96,28 @@ def main():
 if __name__ == "__main__":
    main()
 
-        '''
-        #Test API call
-        headers = {'Content-Type': 'application/json', 'X-Auth-Token': str(token['token_id'])}
-        print headers
-        http = urllib3.PoolManager(
-            cert_reqs = 'CERT_REQUIRED',
-            ca_certs = os.environ['OS_CACERT'])
-        novaListURL = str(token['services'][0]['publicURL']) + "/servers/detail"
-        novaList = http.request(
-            'GET',
-            novaListURL,
-            headers=headers)
-        print novaList.data
-        '''
+'''
+#Test API call
+headers = {'Content-Type': 'application/json', 'X-Auth-Token': str(token['token_id'])}
+print headers
+http = urllib3.PoolManager(
+    cert_reqs = 'CERT_REQUIRED',
+    ca_certs = os.environ['OS_CACERT'])
+novaListURL = str(token['services'][0]['publicURL']) + "/servers/detail"
+novaList = http.request(
+    'GET',
+    novaListURL,
+    headers=headers)
+print novaList.data
+'''
+
+{'services': [
+{'name': u'nova', 'publicURL': u'https://10.75.25.138:13774/v2.1/20187e9020f64f5594e605fde94d8283'},
+ {'name': u'neutron', 'publicURL': u'https://10.75.25.138:13696/'},
+  {'name': u'cinderv2', 'publicURL': u'https://10.75.25.138:13776/v2/20187e9020f64f5594e605fde94d8283'},
+   {'name': u'glance', 'publicURL': u'https://10.75.25.138:13292/'},
+    {'name': u'ceilometer', 'publicURL': u'https://10.75.25.138:13777/'},
+     {'name': u'cinder', 'publicURL': u'https://10.75.25.138:13776/v1/20187e9020f64f5594e605fde94d8283'},
+      {'name': u'heat', 'publicURL': u'https://10.75.25.138:13004/v1/20187e9020f64f5594e605fde94d8283'},
+       {'name': u'swift', 'publicURL': u'https://10.75.25.138:13808/v1/AUTH_20187e9020f64f5594e605fde94d8283'},
+        {'name': u'keystone', 'publicURL': u'https://10.75.25.138:13000/v2.0'}], 'token_id': u'01d62302dfc845efaac3da38efbbf785', 'code': 200, 'expires': u'2017-02-10T18:46:59Z'}
