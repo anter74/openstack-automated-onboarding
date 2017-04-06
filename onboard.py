c86eaf7ce017104fce897dcc3e9989dd4b956cf5
Kyle Greenwell <kyle.greenwell@verizonwireless.com>
2017-04-06 11:04:42 -0400

    Modified templating service and added quota related data

--- onboard.py
+++ onboard.py
@@ -10,22 +10,20 @@ import yaml
 from jinja2 import Environment, PackageLoader
 from osident import osident
 
-#Libraries for dealing with auth token
-import time
-from calendar import timegm
-from datetime import datetime
-
 def main():
     #Create jinja2 environment for rendering heat templates
     env = Environment(loader = PackageLoader('onboard','templates'), trim_blocks=True)
-    template = env.get_template('heat_template.j2')
+    template = env.get_template('template.j2')
     #Read descriptor yaml file
     stream = file('vars.yml', 'r')
     templateVars = yaml.load(stream)
     stream.close()
     #Render jinja2 template to create heat template
-    #print template.render(templateVars)
-
+    stream = open('project.yml', 'w')
+    stream.write(template.render(templateVars))
+    stream.close()
+'''
+    #Create new identity object for OS token management
     identity = osident()
     token = identity.getToken()
     if str(token['code']) == "200":
@@ -33,42 +31,35 @@ def main():
         http = urllib3.PoolManager(
             cert_reqs = 'CERT_REQUIRED',
             ca_certs = os.environ['OS_CACERT'])
-        novaListURL = str(identity.getServiceURL(token,"nova")) + "/os-hypervisors/statistics"
-        novaList = http.request(
+        url = str(identity.getServiceURL(token,"heat")) + "/stacks"
+        request = http.request(
             'GET',
-            novaListURL,
-            headers=headers)
-        print json.loads(novaList.data)
+            url,
+            headers=headers
+        )
+        print request.data
 
     else:
         print "Response Code: %s" % token['code']
         print "Failure Reason: %s" % token['data']
+'''
 
 if __name__ == "__main__":
    main()
 
 '''
-#Test API call
-headers = {'Content-Type': 'application/json', 'X-Auth-Token': str(token['token_id'])}
-print headers
-http = urllib3.PoolManager(
-    cert_reqs = 'CERT_REQUIRED',
-    ca_certs = os.environ['OS_CACERT'])
-novaListURL = str(token['services'][0]['publicURL']) + "/servers/detail"
-novaList = http.request(
-    'GET',
-    novaListURL,
-    headers=headers)
-print novaList.data
-'''
-
-{'services': [
-{'name': u'nova', 'publicURL': u'https://10.75.25.138:13774/v2.1/20187e9020f64f5594e605fde94d8283'},
- {'name': u'neutron', 'publicURL': u'https://10.75.25.138:13696/'},
-  {'name': u'cinderv2', 'publicURL': u'https://10.75.25.138:13776/v2/20187e9020f64f5594e605fde94d8283'},
-   {'name': u'glance', 'publicURL': u'https://10.75.25.138:13292/'},
+{
+  'services': [
+    {'name': u'nova', 'publicURL': u'https://10.75.25.138:13774/v2.1/20187e9020f64f5594e605fde94d8283'},
+    {'name': u'neutron', 'publicURL': u'https://10.75.25.138:13696/'},
+    {'name': u'cinderv2', 'publicURL': u'https://10.75.25.138:13776/v2/20187e9020f64f5594e605fde94d8283'},
+    {'name': u'glance', 'publicURL': u'https://10.75.25.138:13292/'},
     {'name': u'ceilometer', 'publicURL': u'https://10.75.25.138:13777/'},
-     {'name': u'cinder', 'publicURL': u'https://10.75.25.138:13776/v1/20187e9020f64f5594e605fde94d8283'},
-      {'name': u'heat', 'publicURL': u'https://10.75.25.138:13004/v1/20187e9020f64f5594e605fde94d8283'},
-       {'name': u'swift', 'publicURL': u'https://10.75.25.138:13808/v1/AUTH_20187e9020f64f5594e605fde94d8283'},
-        {'name': u'keystone', 'publicURL': u'https://10.75.25.138:13000/v2.0'}], 'token_id': u'01d62302dfc845efaac3da38efbbf785', 'code': 200, 'expires': u'2017-02-10T18:46:59Z'}
+    {'name': u'cinder', 'publicURL': u'https://10.75.25.138:13776/v1/20187e9020f64f5594e605fde94d8283'},
+    {'name': u'heat', 'publicURL': u'https://10.75.25.138:13004/v1/20187e9020f64f5594e605fde94d8283'},
+    {'name': u'swift', 'publicURL': u'https://10.75.25.138:13808/v1/AUTH_20187e9020f64f5594e605fde94d8283'},
+    {'name': u'keystone', 'publicURL': u'https://10.75.25.138:13000/v2.0'}
+  ],
+'token_id': u'01d62302dfc845efaac3da38efbbf785', 'code': 200, 'expires': u'2017-02-10T18:46:59Z'
+}
+'''
